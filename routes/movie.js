144a1e1f9e31bebe84d820cb5779ac605651cfc9
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-09-23 23:59:31 +0300

    /api/movies and /api/movies/movie_id methodları eklendi

--- routes/movie.js
+++ routes/movie.js
@@ -3,6 +3,23 @@ const router = express.Router();
 //Models
 const Movie=require('../Models/Movie');
 
+router.get('/',(req,res)=>{
+const promise=Movie.find({ });
+promise.then((data)=>{
+  res.json(data);
+}).catch((err)=>{
+  res.json(err);
+})
+});
+
+router.get('/:movie_id',(req,res)=>{
+  const  promise=Movie.findById(req.params.movie_id);
+  promise.then((movie)=>{
+    res.json(movie);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
 router.post('/', (req, res, next)=> {
   //const {title,imdb_score,category,country,year}=req.body;
 /*  const movie=new Movie({
