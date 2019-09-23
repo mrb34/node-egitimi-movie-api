cee5d54e898ae95912706ac99c49971a5be007ca
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-09-23 23:34:55 +0300

    api/movies/POST method eklenmesi

--- Helper/db.js
+++ Helper/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 module.exports=()=>{
-    mongoose.connect('mongodb://movieApiUser:MXG750v8040@ds351987.mlab.com:51987/heroku_rntck724',{ useNewUrlParser: true });
+    mongoose.connect('mongodb://movieApiUser:MXG750v8040@ds351987.mlab.com:51987/heroku_rntck724',{ useNewUrlParser: true,useUnifiedTopology: true });
 
     mongoose.connection.on('open',()=>{
     console.log("MongoDB:Connected");
@@ -8,6 +8,7 @@ module.exports=()=>{
     mongoose.connection.on('error',(err)=>{
         console.log("MongoDB:Error",err);
     });
+    mongoose.Promise=global.Promise;
 };
 
 
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+//Models
+const Movie=require('../Models/Movie');
 
+router.post('/', (req, res, next)=> {
+  //const {title,imdb_score,category,country,year}=req.body;
+/*  const movie=new Movie({
+    title:title,
+    imdb_score:imdb_score,
+    category:category,
+    country:country,
+    year:year
+  });
+  */
 
-router.get('/', (req, res, next)=> {
-  res.json({ status: 1});
+  const movie=new Movie(req.body);
+/*  movie.save((err,data)=>{
+    if (err)
+      res.json(err);
+   // res.json(data);
+    res.json(data); //res.json({status:1});
+  });*/
+const promise=movie.save();
+promise.then((data)=>{
+  res.json({status:1});
+}).catch((err)=>{
+  res.json(err);
+});
+
 });
 
 module.exports = router;
