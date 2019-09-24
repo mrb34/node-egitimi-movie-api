c445d521f5fd22feda13118d67a808d4d5552fbd
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-09-25 00:06:11 +0300

    /api/directors/:director_id [GET]

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose=require('mongoose');
 const express = require('express');
 const router = express.Router();
 //Models
@@ -14,7 +15,7 @@ router.post('/', (req, res,next) =>{
 
 });
 
-//////////////////////////////////
+///////////////DIRECTORS LIST///////////////////
 
 router.get('/', (req, res) =>{
   const promise=Director.aggregate([
@@ -43,6 +44,60 @@ router.get('/', (req, res) =>{
         movies:{
           $push:'$movies'
          }
+      }
+    },
+    {
+      $project:{
+        _id:'$_id._id',
+        name:'$_id.name',
+        surname:'$_id.surname',
+        movies:'$movies'
+      }
+    }
+
+  ])
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+
+
+});
+//////////////DIRECTOR LIST WITH ID////////////////
+
+router.get('/:director_id', (req, res) =>{
+  const promise=Director.aggregate([
+    {
+      $match:{
+        '_id':mongoose.Types.ObjectId( req.params.director_id)
+      }
+    },
+    {
+      $lookup:{
+        from:'movies',
+        localField:'_id',
+        foreignField:'director_id',
+        as:'movies'
+      }
+    },
+    {
+      $unwind:{
+        path:'$movies',
+        preserveNullAndEmptyArrays:true
+      }
+    },
+    {
+      $group:{
+        _id:{
+          _id:'$_id',
+          name:'$name',
+          surname:'$surname',
+          bio:'$bio'
+        },
+        movies:{
+          $push:'$movies'
+        }
       }
     },
     {
