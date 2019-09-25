05a686f98d837764f4f37ed10bd62298693fc291
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-09-25 23:32:52 +0300

    /api/director/:director_id [PUT]

--- routes/director.js
+++ routes/director.js
@@ -118,4 +118,23 @@ router.get('/:director_id', (req, res) =>{
 
 
 });
+router.put('/:director_id',(req,res,next)=>{
+  const  promise=Director.findByIdAndUpdate(
+      req.params.director_id,
+      req.body,
+      {
+        new:true
+      }
+  );
+  promise.then((director)=>{
+    if (!director){
+      next({message:'The director was not found',code:4});
+    }else {
+      res.json(director);
+      // res.json({status:4});
+    };
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
 module.exports = router;
